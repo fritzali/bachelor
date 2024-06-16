08081e39eee29b7d70b65a4a54bb11515a9960a2
Fritz Agildere <fritz.agildere@gmail.com>
2024-06-16 16:16:46 +0200

    python

---
+++ plots/main.py
@@ -0,0 +1,9 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+
+arr = np.logspace(0, 1, 1000)
+plt.plot(arr)
+plt.savefig('build/test.pdf')
+plt.close()
