24ef300b51a23419d9e9ecbb83189981ee810212
Fritz Agildere <fritz.agildere@gmail.com>
2024-06-16 17:16:15 +0200

    python

--- plots/main.py
+++ plots/main.py
@@ -3,8 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as const
 
-arr = np.logspace(0, 1, 1000)
-plt.plot(arr)
+arr1 = np.logspace(0, 1, 1000)
+arr2 = np.logspace(0.2, 1.2, 1000)
+arr3 = np.exp(arr1) / 1000
+arr4 = np.sqrt(arr2)
+
+plt.plot(arr1, label='log')
+plt.plot(arr2, label='log')
+plt.plot(arr3, label='exp')
+plt.plot(arr4, label='sqrt')
+
+plt.xlabel(r'$x \mathrel{/} $')
+plt.ylabel(r'$y \mathrel{/} $')
+
+plt.legend()
+
 plt.savefig('build/test.pdf')
 plt.savefig('build/test.jpg')
 plt.close()
